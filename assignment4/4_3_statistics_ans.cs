7dcffa9ee25f97f159e969d4d42cd2e174f48e1b
gpzofficial <57559311+gpzofficial@users.noreply.github.com>
2024-11-06 23:59:58 +0900

    Update 4_3_statistics_ans.cs
    done

--- assignment4/4_3_statistics_ans.cs
+++ assignment4/4_3_statistics_ans.cs
@@ -21,6 +21,63 @@ namespace statistics
             int stdCount = data.GetLength(0) - 1;
             // ---------- TODO ----------
             
+            Console.WriteLine("Average Scores:");
+            double outputData;
+            for(int j = 2; j < 5; j++)
+            {
+                outputData = 0;
+                for(int i = 1; i <= stdCount; i++) {
+                    outputData += double.Parse(data[i, j]);
+                }
+                outputData /= stdCount;
+                Console.WriteLine($"{data[0, j]}: {Math.Round(outputData, 2)}");
+            }
+            
+            Console.WriteLine("");
+            double maxData;
+            double minData;
+            Console.WriteLine("Max and min Scores:");
+            for(int j = 2; j < 5; j++)
+            {
+                maxData = 0;
+                minData = 100;
+                for(int i = 1; i <= stdCount; i++) {
+                    if(minData >= double.Parse(data[i, j])) {
+                        minData = double.Parse(data[i, j]);
+                    }
+                    if(maxData <= double.Parse(data[i, j])) {
+                        maxData = double.Parse(data[i, j]);
+                    }
+                }
+                
+                Console.WriteLine($"{data[0, j]}: ({maxData}, {minData})");
+            }
+            
+            Console.WriteLine("");
+            double currentData;
+            int rank;
+            Console.WriteLine("Students rank by total scores:");
+            for(int j = 1; j <= stdCount; j++)
+            {
+                rank = 1;
+                currentData = double.Parse(data[j, 2]) + double.Parse(data[j, 3]) + double.Parse(data[j, 4]);
+                for(int i = 1; i <= stdCount; i++) {
+                    if(i != j) {
+                        if(currentData <= double.Parse(data[i, 2]) + double.Parse(data[i, 3]) + double.Parse(data[i, 4])) {
+                            rank++;
+                        }
+                    }
+                    
+                }
+                
+                string prefix = "th";
+                if(rank == 1) prefix = "st";
+                else if(rank == 2) prefix = "nd";
+                else if(rank == 3) prefix = "rd";
+                Console.WriteLine($"{data[j, 1]}: {rank}{prefix}");
+            }
+            
+            
             // --------------------
         }
     }
